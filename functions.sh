ee10a2e109fe1d69b32733b1eec193b23692f23a
Jenkins Backup Script <build-engeneer@my-company.com>
2015-04-04 18:19:38 +0000

    change colored output, double quote automated commit message

--- functions.sh
+++ functions.sh
@@ -4,43 +4,51 @@
 #
 
 
+nocolor='\e[0m'
+green='\e[0;32m'
+red='\e[0;31m'
+blue='\e[0;34m'
+
 # echo in green
-function echo_task {
-    green='\e[0;32m'
-    nocolor='\e[0m'
+function echo_green() {
     echo -e "${green}[$(date)] $@ ${nocolor}"
 }
 
-echo in red
-function alert {
-    red='\e[0;31m'
-    nocolor='\e[0m'
+#echo in blue
+function echo_blue() {
+    echo -e "${blue}[$(date)] $@ ${nocolor}"
+}
+#echo in red
+function echo_red() {
     echo -e "${red}[$(date)] $@ ${nocolor}"
 }
 
 
 ## usage
 usage() {
-  echo "USAGE: "
-  echo "$0 GIT_REPO GIT_ACCOUNT"
+  echo_blue "USAGE: "
+  echo_blue  " $0 GIT_REPO GIT_ACCOUNT"
 }
 
 
 ## rsync to back directory
 syncToBackup() {
-  echo_task "Syncing $JENKINS_HOME to $BACKUP_DIR"
-  rsync -avHx $JENKINS_HOME/* $BACKUP_DIR
+  echo_green "Syncing $JENKINS_HOME to $BACKUP_DIR"
+  echo_green "$(rsync -avHx $JENKINS_HOME/* $BACKUP_DIR)"
 }
 
 ## pushing it all to the git repo
 gitCommitPUsh() {
     git config --global user.name "$GIT_USER"
-    git config --global user.name "$GIT_EMAIL"
-    echo_task "Adding new files to git"
-    git add -v --all ./*
-    echo_task "Commiting into local git"
-    git commit -m "$COMMIT_MESSAGE"
-    echo_task "Pushing into master git"
-    git push origin master
-}
+    git config --global user.email "$GIT_EMAIL"
+    echo_green "Adding new files to git"
+    echo -ne "${green}"; git  add --verbose  --all $BACKUP_DIR/; echo -ne "${nocolor}"
+    ### commit message must be quoted with double quotes, otherwise: ERROR! fatal  
+    echo_green "Commiting into local git $(pwd)"
+    echo -ne "${green}"
+    git commit --verbose -m "$COMMIT_MESSAGE"
+    echo -ne "${nocolor}"
 
+    echo_green "Pushing into master git"
+    echo -ne "${green}"; $(git push --verbose origin master); echo -ne "${nocolor}"
+}
